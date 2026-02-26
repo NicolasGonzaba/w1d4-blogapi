e33c38c3ab6dabecc67d0538416be32994b46ae8
NicolasGonzaba <nicolasgonzaba1@gmail.com>
2026-02-26 08:51:01 -0800

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace w1d4_blogapi;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
